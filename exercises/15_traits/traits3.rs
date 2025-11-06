c0170f013bd7ed6c18381f98fe5c05ac852e88f2
vq-x <tylerv2003@gmail.com>
2025-11-06 12:25:28 -0500

    progress

--- exercises/15_traits/traits3.rs
+++ exercises/15_traits/traits3.rs
@@ -3,8 +3,12 @@ trait Licensed {
     // implementors like the two structs below can share that default behavior
     // without repeating the function.
     // The default license information should be the string "Default license".
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Default license")
+    }
 }
+
+
 
 struct SomeSoftware {
     version_number: i32,
